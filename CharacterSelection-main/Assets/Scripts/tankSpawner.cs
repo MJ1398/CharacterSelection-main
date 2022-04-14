52559526528044401caaebd022f17f889f4e5785
MJ1398 <49242008+MJ1398@users.noreply.github.com>
2022-04-14 09:50:50 +0530

    MVC Implemented
    Connected the controller script to view and model script with the help of MVC

--- CharacterSelection-main/Assets/Scripts/tankSpawner.cs
+++ CharacterSelection-main/Assets/Scripts/tankSpawner.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tankSpawner : MonoBehaviour
+public class TankSpawner : MonoBehaviour
 {
-    public GameObject tankPrefab;
+    public TankView tankView;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+        CreateTank();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateTank()
     {
-        
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankModel, tankView);
     }
 }
